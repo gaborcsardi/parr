df065945f9f07881d62cbb3aefe283501c3b45fb
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-28 22:46:10 +0000

    Simplify initialization, no mc clusters...

--- R/zzz.R
+++ R/zzz.R
@@ -18,17 +18,6 @@
 
 .noGenerics <- TRUE
 
-if (.Platform$OS.type == "windows")
-    utils::globalVariables(c("mc_pids", "clean_pids"), add = TRUE)
-
-## dummy, just so we can register a finalizer
-.fin.env <- new.env()
-
-.onLoad <- function(libname, pkgname)
-{
-    initDefaultClusterOptions(libname)
-    cores <- getOption("mc.cores", NULL)
-    if(is.null(cores) && !is.na(nc <- as.integer(Sys.getenv("MC_CORES"))))
-        options("mc.cores" = nc)
-#    if(.Platform$OS.type == "unix") reg.finalizer(.fin.env, clean_pids, TRUE)
+.onLoad <- function(libname, pkgname) {
+  initDefaultClusterOptions(libname)
 }
